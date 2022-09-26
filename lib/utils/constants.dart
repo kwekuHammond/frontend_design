ba8e1107fac2f271e3c82858b4847b21d8c9f54a
kwekuhammond <hammondkweku@gmail.com>
2022-09-26 02:55:04 +0000

    adds first login view design

---
+++ lib/utils/constants.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/animation.dart';
+
+const Color primaryColor = Color.fromARGB(255, 36, 112, 198);
--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_design/utils/constants.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,6 +11,150 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    Size screenSize = MediaQuery.of(context).size; //Gets total size of screen
+    return SafeArea(
+      child: Scaffold(
+        // resizeToAvoidBottomInset: false,
+        body: Stack(
+          children: [
+            //FIRST BLACK LAYER
+            Container(
+              height: screenSize.height,
+              width: screenSize.width,
+              color: Colors.black,
+            ),
+            Container(
+              //SENCOND BLUE LAYER
+              width: screenSize.width,
+              height: screenSize.height * 0.65,
+              decoration: const BoxDecoration(
+                color: primaryColor,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.only(top: 50),
+                    child: const Center(
+                      child: Text(
+                        "Firebase",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: 'Romans',
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    // LOGIN CONTAINER
+                    child: Container(
+                      height: screenSize.height * 0.65,
+                      width: screenSize.width * 0.85,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          const Center(
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 40),
+                              child: Text(
+                                "Login",
+                                style: TextStyle(
+                                  fontSize: 35,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(
+                              right: 30,
+                              left: 30,
+                              top: 100,
+                            ),
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              spacing: 30,
+                              runSpacing: 20,
+                              children: [
+                                const TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Email",
+                                    hintStyle: TextStyle(
+                                      color: primaryColor,
+                                    ),
+                                  ),
+                                ),
+                                const TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(
+                                      color: primaryColor,
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 40),
+                                  child: ElevatedButton(
+                                    onPressed: () {},
+                                    child: const Text("Sign in"),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Don't have an account?",
+                            textAlign: TextAlign.center,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {},
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                color: primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
